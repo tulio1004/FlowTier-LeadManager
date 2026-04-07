dbca787e47317a9ca013e844d8d9bba5b90244c1
tulio1004 <248964784+tulio1004@users.noreply.github.com>
2026-04-07 12:15:53 -0400

    config: add GHL_API_KEY and GHL_LOCATION_ID to pm2 ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,9 @@ module.exports = {
     env: {
       PORT: 4000,
       ADMIN_USER: 'tulio',
-      ADMIN_PASS: '25524515Fl0wT13r'
+      ADMIN_PASS: '25524515Fl0wT13r',
+      GHL_API_KEY: 'pit-cecf9555-ca25-4ce8-aa4d-c2d824a73462',
+      GHL_LOCATION_ID: 'pljMXE9pDrk5o2nEKJGq'
     }
   }]
 };
